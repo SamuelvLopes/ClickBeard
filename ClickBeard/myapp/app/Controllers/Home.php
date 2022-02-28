c1b109c2097456b27f3d7c90b54b89abd46ec244
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-02-27 22:18:34 -0300

    template escolhido

--- ClickBeard/myapp/app/Controllers/Home.php
+++ ClickBeard/myapp/app/Controllers/Home.php
@@ -2,14 +2,19 @@
 
 namespace App\Controllers;
 use App\Models\Basic;
+use App\Libraries\Template;
 
 class Home extends BaseController
 {
     public function index()
     {
-        $teste=new \App\Models\Teste();
-        $p=$teste->where('ID',1)->findAll();
-        var_dump($p);
-        echo "teste...";
+
+        $template=new Template();
+        $data=["d"];
+        $template->show("teste",  $data);
+        //$teste=new \App\Models\Teste();
+        //$p=$teste->where('ID',1)->findAll();
+        //var_dump($p);
+      
     }
 }
